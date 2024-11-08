23f07debe7c2bcadd7a4958369994793d5483757
James Casey <hanavi@gmail.com>
2024-11-07 23:26:15 -0500

    Fix some issues with the bash install script

--- scripts/bash_install.sh
+++ scripts/bash_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Author: James Casey
-# Last Updated: 2024-10-22
+# Last Updated: 2024-11-07
 
-echo "[ WARNING ] Running in test mode"
+# echo "[ WARNING ] Running in test mode"
 
 create_link() {
 
@@ -10,11 +10,15 @@ create_link() {
     dst=$2
 
     if [ ! -e "${dst}" ]; then
-        echo ln -s "${src}" "${dst}"
+        ln -s "${src}" "${dst}"
     else
         printf "skipping %s\n" "${dst}"
     fi
 }
+
+[[ -d ~/.config ]] || mkdir ~/.config
+[[ -f ~/.bashrc ]] || printf '[[ -f ~/.bash_aliases ]] && . ~/.bash_aliases\n' > ~/.bashrc
+[[ -f ~/.bash_profile ]] || printf '[[ -f ~/.bashrc ]] && . ~/.bashrc\n' > ~/.bash_profile
 
 create_link ~/files/dotfiles/bash_aliases ~/.bash_aliases
 create_link ~/files/dotfiles/inputrc ~/.inputrc
