c95d1d37359ddc5a4c34f6f7778ca7f41bd08a20
James Casey <hanavi@gmail.com>
2025-03-20 21:01:07 -0400

    Add conda bash aliases

---
+++ bash/conda.bash
@@ -0,0 +1,9 @@
+[[ -n "$_BASH_DEBUG" ]] && echo "loading conda plugin"
+
+alias c='conda'
+alias cel='conda env list'
+alias caw='conda activate working'
+
+alias ci='conda install'
+alias ciy='conda install -y'
+
